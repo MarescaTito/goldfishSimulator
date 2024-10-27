b0d2d32636bbb10db28bb3b082abe42d9057e942
MarescaTito <85365989+MarescaTito@users.noreply.github.com>
2024-10-27 17:33:25 -0400

    Final Results

--- src/main/java/org/cheeberts/Main.java
+++ src/main/java/org/cheeberts/Main.java
@@ -1,8 +1,7 @@
 package org.cheeberts;
 
 import org.cheeberts.constants.deckList.*;
-import org.cheeberts.model.Card;
-import org.cheeberts.service.GoldfishSimulator;
+  import org.cheeberts.service.GoldfishSimulator;
 import org.cheeberts.util.SmoothedDeck;
 
 public class Main {
@@ -12,8 +11,8 @@ public class Main {
             if(i%10 == 0) {
                 System.out.println(i);
             }
-            winTurns[GoldfishSimulator.turnsToWin(SmoothedDeck.getSmoothedDeck(DuskmourneLeyline.getDeck(),
-                    DuskmourneLeyline.landRatio()))]++;
+            winTurns[GoldfishSimulator.turnsToWin(SmoothedDeck.getSmoothedDeck(DuskmourneLeylineSubDreadmaw.getDeck(),
+                    DuskmourneLeylineSubDreadmaw.landRatio()))]++;
         }
 
         for(int i : winTurns) {
